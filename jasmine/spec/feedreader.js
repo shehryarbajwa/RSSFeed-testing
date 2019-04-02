3d31ac6ada34183f09b09256a45f47fa1834f652
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-04-01 21:33:06 -0400

    name and url tests written

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,10 +32,10 @@ $(function() {
          * and that the URL is not empty.
          */
 
-         it('url and name defined', function(){
+         it('url defined', function(){
             for (let feed of allFeeds){
               expect(feed.url).toBeDefined();
-              expect(feed.name).toBeDefined();
+              expect(feed.url.length).not.toBe(0);
             }
          });
 
@@ -44,6 +44,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         it('name defined', function(){
+           for(let feed of allFeeds){
+             expect(feed.name).toBeDefined();
+             expect(feed.name.length).not.toBe(0);
+           }
+         })
     });
 
 
--- js/app.js
+++ js/app.js
@@ -9,14 +9,14 @@
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [
     {
-        
+        name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
     }, {
         name: 'CSS Tricks',
         url: 'http://feeds.feedburner.com/CssTricks'
     }, {
         name: 'HTML5 Rocks',
-        url: 'https://shehryar'
+        url: 'htp://feeds.feedburner.com/html5rocks'
     }, {
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
